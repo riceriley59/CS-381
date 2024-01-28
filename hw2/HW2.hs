de343e20e2bd4f79231d26782c4204c9789689a4
Riley Rice <riceriley59@gmail.com>
2024-01-28 14:03:57 -0800

    Added function prototypes

--- hw2/HW2.hs
+++ hw2/HW2.hs
@@ -5,3 +5,19 @@ import HW2types
 sizeTree :: Tree -> Int
 sizeTree Leaf = 0 
 sizeTree (Node _ l r) = sizeTree l + sizeTree r + 1  
+
+height :: Tree -> Int
+
+treeSum :: Tree -> Int 
+
+mergeTrees :: Tree Tree -> Tree 
+
+isBst :: Tree -> Bool
+
+convertBst :: Tree -> Tree
+
+numVE :: Graph -> (Int, Int)
+
+removeLoops :: Graph -> Graph
+
+removeVertex :: Vertex Graph -> Graph
